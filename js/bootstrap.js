e0e212e7a2fec0a068210c515870b171c9ffab97
namiltd <namiltd@users.noreply.github.com>
2018-04-25 19:32:36 +0200

    source !== Object(source) test

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -45,7 +45,8 @@
 
   function _objectSpread(target) {
     for (var i = 1; i < arguments.length; i++) {
-      var source = arguments[i] != null ? arguments[i] : {};
+      var source = arguments[i];
+      if (source !== Object(source)) continue;
       var ownKeys = Object.keys(source);
 
       if (typeof Object.getOwnPropertySymbols === 'function') {
