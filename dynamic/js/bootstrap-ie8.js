426f4ecc742babf1bf67e200893435dd57df4ea3
namiltd <namiltd@users.noreply.github.com>
2018-06-25 13:32:41 +0200

    remove querySelector and querySelectorAll

--- dynamic/js/bootstrap-ie8.js
+++ dynamic/js/bootstrap-ie8.js
@@ -134,37 +134,6 @@ if (!window.Node){
 }());
 
 (function() {
-  if (!document.querySelectorAll) {
-    document.querySelectorAll = function (selectors) {
-      var style = document.createElement('style'), elements = [], element;
-      document.documentElement.firstChild.appendChild(style);
-      document._qsa = [];
-
-      style.styleSheet.cssText = selectors + '{x-qsa:expression(document._qsa && document._qsa.push(this))}';
-      window.scrollBy(0, 0);
-      style.parentNode.removeChild(style);
-
-      while (document._qsa.length) {
-        element = document._qsa.shift();
-        element.style.removeAttribute('x-qsa');
-        elements.push(element);
-      }
-      document._qsa = null;
-      return elements;
-    };
-  }
-}());
-
-(function() {
-  if (!document.querySelector) {
-    document.querySelector = function (selectors) {
-      var elements = document.querySelectorAll(selectors);
-      return (elements.length) ? elements[0] : null;
-    };
-  }
-}());
-
-(function() {
   var _slice = Array.prototype.slice;
   Array.prototype.slice = function() {
     if(this instanceof Array) {
